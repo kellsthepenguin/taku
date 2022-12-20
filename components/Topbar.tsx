43594de6eb22b77b2cfbeb6ed785a3522d8c5c30
Kim <jwkim101201@gmail.com>
2022-12-20 18:04:32 +0900

    feat: dark mode

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,20 +1,46 @@
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useRef, useState } from 'react'
+
 export default function Topbar() {
+  const [isDarkMode, setDarkMode] = useState(false)
+  const firstUpdate = useRef(true)
+
+  useEffect(() => {
+    if (localStorage.isDarkMode === 'true') {
+      setDarkMode(true)
+    } else {
+      setDarkMode(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false
+      return
+    }
+
+    if (typeof window !== 'undefined') {
+      if (isDarkMode) {
+        document.documentElement.classList.add('dark')
+        localStorage.isDarkMode = 'true'
+      } else {
+        document.documentElement.classList.remove('dark')
+        localStorage.isDarkMode = 'false'
+      }
+    }
+  }, [isDarkMode])
+
   return (
-    <nav className='mx-2 my-2 rounded-md flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-5 bg-white sm:items-baseline'>
+    <nav className='mx-2 my-2 rounded-md flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-5 bg-white dark:bg-slate-900 sm:items-baseline dark:text-white font-semibold'>
       <div className='mb-2 sm:mb-0'>
-        <a href='/home' className='text-2xl no-underline text-grey-darkest hover:text-blue-dark'>
+        <a href='/home' className='text-2xl no-underline'>
           <img src='/taku.svg' width={70} height={35}/>
         </a>
         </div>
       <div>
-        <a href='/one' className='text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2'>
-          One
-        </a>
-        <a href='/two' className='text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2'>
-          Two
-        </a>
-        <a href='/three' className='text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2'>
-          Three
+        <a onClick={() => setDarkMode(!isDarkMode)} className='text-lg no-underline ml-2 text-gray-600 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2.5'>
+          {isDarkMode ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
         </a>
       </div>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='absolute flex flex-col h-[100%] w-[100%]'>
       <Topbar />
-      <div className='rounded-md bg-white mx-2 mb-2 py-5 flex-1'>
+      <div className='rounded-md bg-white dark:bg-slate-900 mx-2 mb-2 py-5 flex-1'>
         
       </div>
     </div>
