9a9de14b6e6b00a5807a45612753df9431e1fddd
Kim <jwkim101201@gmail.com>
2022-12-23 07:46:04 +0900

    feat: add user page

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,13 +1,17 @@
+import { faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 interface ProfileProps {
   photoURL: string
   name: string
+  isLocked: boolean
 }
 
-export default function Profile({ photoURL, name }: ProfileProps) {
+export default function Profile({ photoURL, name, isLocked }: ProfileProps) {
   return (
     <div className='rounded bg-slate-300 dark:bg-slate-800 h-40 w-28 flex flex-col items-center hover:bg-slate-400 dark:hover:bg-slate-700'>
       <img src={photoURL} height={90} width={90} className='mt-[11px] rounded' />
-      <p className='mt-4 text-[13pt] font-semibold'>{name}</p>
+      <p className='mt-4 text-[13pt] font-semibold'>{name} {isLocked && <FontAwesomeIcon icon={faLock} />}</p>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import Frame from '../components/Frame'
 import Profile from '../components/Profile'
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function Login() {
+  const { data, error, isLoading } = useSWR('/api/users', fetcher)
+
   return (
     <Frame>
       <div className='text-center mt-24'>
         <div className='inline-block text-left'>
           <p className='mb-2 text-xl font-semibold dark:text-white'>Select Profile</p>
           <div className='flex items-center justify-center gap-3'>
-            <Profile photoURL='https://opgg-static.akamaized.net/meta/images/lol/champion/Teemo.png' name='teemo' />
-            <Profile photoURL='https://opgg-static.akamaized.net/meta/images/lol/champion/Tristana.png' name='tristana' />
+            {
+              isLoading ? <p>Loading...</p> : error ? <p>Error</p> : data.map((data: any) => <Profile name={data.name} photoURL={data.photoURL} isLocked={data.isLocked} />)
+            }
           </div>
         </div>
       </div>
