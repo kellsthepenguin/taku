ab0b9394a1ef607f9016b669740c953cdc5ccdb1
Kim <jwkim101201@gmail.com>
2022-12-23 15:57:22 +0900

    feat: add add user button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Frame from '../components/Frame'
 import Profile from '../components/Profile'
 import useSWR from 'swr'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
@@ -11,11 +13,12 @@ export default function Login() {
     <Frame>
       <div className='text-center mt-24'>
         <div className='inline-block text-left'>
-          <p className='mb-2 text-xl font-semibold dark:text-white'>Select Profile</p>
+          <p className='mb-2 text-xl font-bold dark:text-white'>Select Profile</p>
           <div className='flex items-center justify-center gap-3'>
             {
               isLoading ? <p>Loading...</p> : error ? <p>Error</p> : data.map((data: any) => <Profile name={data.name} photoURL={data.photoURL} isLocked={data.isLocked} />)
             }
+            <Profile name='Add user' photoURL='/circle-plus-solid.svg' isLocked={false} />
           </div>
         </div>
       </div>
