8b6c912dbad60ff6d2630bcec6cb11b1c75bc1f6
Kim <jwkim101201@gmail.com>
2022-12-27 17:35:20 +0900

    improve: emphasize add user button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Frame from '../components/Frame'
 import Profile from '../components/Profile'
 import useSWR from 'swr'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
@@ -16,7 +18,10 @@ export default function ProfileSelect() {
             {
               isLoading ? <p>Loading...</p> : error ? <p>Error</p> : data.map((data: any) => <Profile name={data.name} photoURL={data.photoURL} isLocked={data.isLocked} />)
             }
-            <Profile name='Add user' photoURL='/circle-plus-solid.svg' isLocked={false} className='bg-slate-800 dark:bg-slate-300 hover:bg-slate-700 dark:hover:bg-slate-400 text-white dark:text-black' imgClassName='invert' />
+            <div className='rounded bg-slate-800 dark:bg-slate-300 h-40 w-28 flex flex-col items-center hover:bg-slate-400 dark:hover:bg-slate-400 text-white dark:text-black'>
+              <FontAwesomeIcon icon={faCirclePlus} className='mt-[11px] rounded dark:text-slate-900 h-[90px] w-[90px]' />
+              <p className='mt-4 text-[13pt] font-semibold'>Add user</p>
+            </div>
           </div>
         </div>
       </div>
