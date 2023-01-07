bf8d8f504150ae938c406d63231b44878c58552f
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-01-08 08:43:32 +0900

    style: 20px -> 19px

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -2,6 +2,6 @@ import { PropsWithChildren } from 'react'
 
 export default function PrimaryButton({ children }: PropsWithChildren) {
   return (
-    <button type='button' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>{children}</button>
+    <button type='button' className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-[19px] py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>{children}</button>
   )
 }
