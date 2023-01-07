d8a942effbc357094b5d2ee7e019d54dd55cf53a
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-01-07 09:58:03 +0900

    fix: make profile selection div center

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,9 @@ export default function ProfileSelect() {
 
   return (
     <Frame>
-      <div className='text-center mt-24'>
+      <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
         <div className='inline-block text-left'>
-          <p className='mb-2 text-xl font-bold dark:text-white'>Select Profile</p>
+          <p className='text-xl font-bold dark:text-white'>Select Profile</p>
           <div className='flex items-center justify-center gap-3'>
             {
               isLoading ? <p>Loading...</p> : error ? <p>Error</p> : data.map((data: any) => <Profile name={data.name} photoURL={data.photoURL} isLocked={data.isLocked} />)
